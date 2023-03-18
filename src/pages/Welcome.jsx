186e9543bc36a3d5278d2947bd7fb8a39a5f6047
ri1ken <719401217@qq.com>
2023-03-18 22:44:07 +0900

    chore: default welcome page

---
+++ src/pages/Welcome.jsx
@@ -0,0 +1,8 @@
+export default function Welcome() {
+  return (
+    <div className="bg-sky-400 mx-4 w-full flex flex-col items-center">
+      <h1>DailyGPT</h1>
+      <span className="italic">Use AI as our strong daily tool!</span>
+    </div>
+  );
+};
