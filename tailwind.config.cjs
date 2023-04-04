22ed3181cc82739f1be812e87bf9005abe9ef11d
ri1ken <719401217@qq.com>
2023-04-04 23:02:20 +0900

    chore: reconfig font size

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,16 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontSize: {
+      "xxs": "0.5rem",
+      "xs": "0.75rem",
+      "sm": "0.875rem",
+      "base": "1rem",
+      "lg": "0.875rem",
+      "xl": "1.25rem",
+      "2xl": "1.5rem",
+      "3xl": "1.875rem",
+    },
     extend: {},
   },
   plugins: [],
